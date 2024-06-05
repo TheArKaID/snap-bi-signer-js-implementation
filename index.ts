837f19a4407d50c349c205ea52424f08335bd3fd
Arifia Kasastra R <arka.progammer@gmail.com>
2024-06-05 10:19:45 +0700

    chore: update snap-bi-signer dependency version to 0.1.0

--- index.ts
+++ index.ts
@@ -14,105 +14,137 @@ const envVariables = {
     PJP_PUBLIC_KEY: ''
 };
 
-function getUserInput() {
-    checkEnvVariables();
+function runner() {
     (async () => {
-        let input = await rla(`Choose an option: \n1. Asymmetric Signature\n2. Verify Asymmetric Signature\n3. Symmetric Signature\n4. Verify Symmetric Signature\nType 'exit' to close the program\n`);
-        let signature: string;
-        let timestamp: string;
-        let result: boolean;
-        let httpMethod: string;
-        let url: string;
-        let body: string;
-        let accessToken: string;
-
-        switch (input) {
-            case '1':
-                console.log('Asymmetric Signature: \n');
-                console.log(asymmetricSignature(envVariables.CLIENT_ID, envVariables.PRIVATE_KEY));
-                console.log('\n');
-                rl.question('Press any key to continue', () => {
-                    return getUserInput();
-                });
-                break;
-            case '2':
-                console.log('Verify Asymmetric Signature: \n');
-                signature = await rla('Enter the signature: ');
-                timestamp = await rla('Enter the timestamp: ');
-                console.log('Result: ');
-                result = verifyAsymmetricSignature({clientID: envVariables.CLIENT_ID, publicKey: envVariables.PJP_PUBLIC_KEY, signature, timestamp});
-
-                if (result) {
-                    // Signature is valid with green color
-                    console.log('\x1b[32m', 'Signature is valid');
-                    // Reset the color
-                    console.log('\x1b[0m');
-                } else {
-                    console.log('\x1b[31m', 'Signature is invalid')
-                    console.log('\x1b[0m');
-                }
-                console.log('\n');
-                rl.question('Press any key to continue', () => {
-                    return getUserInput();
-                });
-                break;
-            case '3':
-                // Input for symmetric signature
-                console.log('Symmetric Signature: \n');
-                httpMethod = await rla('Enter the HTTP method: ');
-                url = await rla('Enter the URL: ');
-                timestamp = await rla('Enter the timestamp: ');
-                body = await rla('Enter the body: ');
-                accessToken = await rla('Enter the AccessToken: ');
-                signature = symmetricSignature({clientSecret: envVariables.CLIENT_SECRET, httpMethod, relativeUrl: url, timestamp, requestBody: JSON.parse(body), accessToken});
-
-                console.log('Signature: \n');
-                console.log(signature);
-                console.log('\n');
-                rl.question('Press any key to continue', () => {
-                    return getUserInput();
-                });
-                break;
-            case '4':
-                // Input for verifying symmetric signature
-                console.log('Verify Symmetric Signature: \n');
-                signature = await rla('Enter the signature: ');
-                httpMethod = await rla('Enter the HTTP method: ');
-                url = await rla('Enter the URL: ');
-                timestamp = await rla('Enter the timestamp: ');
-                body = await rla('Enter the body: ');
-                accessToken = await rla('Enter the AccessToken: ');
-
-                console.log('Result: ');
-                result = verifySymmetricSignature({clientSecret: envVariables.CLIENT_SECRET, signature, httpMethod, relativeUrl: url, timestamp: timestamp, requestBody: JSON.parse(body), accessToken});
-
-                if (result) {
-                    // Signature is valid with green color
-                    console.log('\x1b[32m', 'Signature is valid');
-                    // Reset the color
-                    console.log('\x1b[0m');
-                } else {
-                    console.log('\x1b[31m', 'Signature is invalid')
-                    console.log('\x1b[0m');
-                }
-                console.log('\n');
-                rl.question('Press any key to continue', () => {
-                    return getUserInput();
-                });
-                break;
-            case 'exit':
-                console.log('\nExiting the program. Bye~\n');
-                process.exit(0);
-                break;
-            default:
-                console.log('\nInvalid input\n')
-                getUserInput();
-                break;
+        try {
+            await checkEnvVariables();
+            await userInteraction();
+        } catch (error: any) {
+            console.log('\x1b[31m', 'Fatal error. Please check your environment variables and try again');
+            console.log('\x1b[0m');
+            console.error(error);
+            process.exit(1);
         }
-    })();
+    })()
 }
 
-function checkEnvVariables() {
+async function userInteraction() {
+    let input = await rla(`Choose an option: \n1. Asymmetric Signature\n2. Verify Asymmetric Signature\n3. Symmetric Signature\n4. Verify Symmetric Signature\nType 'exit' to close the program\n`);
+    let signature: string;
+    let timestamp: string;
+    let result: boolean;
+    let httpMethod: string;
+    let url: string;
+    let body: string;
+    let accessToken: string;
+
+    switch (input) {
+        case '1':
+            console.log('Asymmetric Signature: \n');
+            console.log(asymmetricSignature({
+                clientID: envVariables.CLIENT_ID, privateKey: envVariables.PRIVATE_KEY
+            }));
+            console.log('\n');
+            rl.question('Press any key to continue', () => {
+                return userInteraction();
+            });
+            break;
+        case '2':
+            console.log('Verify Asymmetric Signature: \n');
+            signature = await rla('Enter the signature: ');
+            timestamp = await rla('Enter the timestamp: ');
+            console.log('Result: ');
+            result = verifyAsymmetricSignature({
+                clientID: envVariables.CLIENT_ID,
+                publicKey: envVariables.PJP_PUBLIC_KEY, signature, timestamp
+            });
+
+            if (result) {
+                // Signature is valid with green color
+                console.log('\x1b[32m', 'Signature is valid');
+                // Reset the color
+                console.log('\x1b[0m');
+            } else {
+                console.log('\x1b[31m', 'Signature is invalid')
+                console.log('\x1b[0m');
+            }
+            console.log('\n');
+            rl.question('Press any key to continue', () => {
+                return userInteraction();
+            });
+            break;
+        case '3':
+            // Input for symmetric signature
+            console.log('Symmetric Signature: \n');
+            httpMethod = await rla('Enter the HTTP method: ');
+            url = await rla('Enter the URL: ');
+            timestamp = await rla('Enter the timestamp: ');
+            body = await rla('Enter the body: ');
+            accessToken = await rla('Enter the AccessToken: ');
+            signature = symmetricSignature({
+                clientSecret: envVariables.CLIENT_SECRET,
+                httpMethod,
+                relativeUrl: url,
+                timestamp,
+                requestBody: JSON.parse(body),
+                accessToken
+            });
+
+            console.log('Signature: \n');
+            console.log(signature);
+            console.log('\n');
+            rl.question('Press any key to continue', () => {
+                return userInteraction();
+            });
+            break;
+        case '4':
+            // Input for verifying symmetric signature
+            console.log('Verify Symmetric Signature: \n');
+            signature = await rla('Enter the signature: ');
+            httpMethod = await rla('Enter the HTTP method: ');
+            url = await rla('Enter the URL: ');
+            timestamp = await rla('Enter the timestamp: ');
+            body = await rla('Enter the body: ');
+            accessToken = await rla('Enter the AccessToken: ');
+
+            console.log('Result: ');
+            result = verifySymmetricSignature({
+                clientSecret: envVariables.CLIENT_SECRET,
+                httpMethod,
+                relativeUrl: url,
+                accessToken,
+                requestBody: JSON.parse(body),
+                timestamp,
+                signature
+                
+            });
+
+            if (result) {
+                // Signature is valid with green color
+                console.log('\x1b[32m', 'Signature is valid');
+                // Reset the color
+                console.log('\x1b[0m');
+            } else {
+                console.log('\x1b[31m', 'Signature is invalid')
+                console.log('\x1b[0m');
+            }
+            console.log('\n');
+            rl.question('Press any key to continue', () => {
+                return userInteraction();
+            });
+            break;
+        case 'exit':
+            console.log('\nExiting the program. Bye~\n');
+            process.exit(0);
+            break;
+        default:
+            console.log('\nInvalid input\n')
+            userInteraction();
+            break;
+    }
+}
+
+async function checkEnvVariables() {
     if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PRIVATE_KEY || !process.env.PJP_PUBLIC_KEY) {
         console.error('Please set the CLIENT_ID, CLIENT_SECRET, PRIVATE_KEY, and PJP_PUBLIC_KEY environment variables');
         process.exit(1);
@@ -124,4 +156,4 @@ function checkEnvVariables() {
     envVariables.PJP_PUBLIC_KEY = Buffer.from(process.env.PJP_PUBLIC_KEY, 'base64').toString('ascii');
 }
 
-getUserInput();
+runner();
